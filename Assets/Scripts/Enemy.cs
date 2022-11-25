c5cdaff74d736d9bcfed84b0cb0477b40e0faf06
underscoren <marius.steffens@outlook.com>
2022-11-25 14:35:03 +0000

    Created enemies

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 3.5f;
+
+    void Update() {
+        var pos = transform.position;
+        pos -= new Vector3(0,_speed * Time.deltaTime,0);
+        transform.position = pos;
+
+        if (pos.y < -10)
+            Destroy(this);
+    }
+
+    public void OnTriggerEnter2D(Collider2D other) {
+        if(other.CompareTag("Player")) {
+            Destroy(other);
+        }
+    }
+}
